4c9d3b72123ccfa55ac1dd2353365f5080882c24
mireya111 <mireya.garcia@epn.edu.ec>
2025-01-03 19:01:30 -0500

    Modificación_del_puerto_para_correo

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -8,7 +8,7 @@ dotenv.config();
 let transportador = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
