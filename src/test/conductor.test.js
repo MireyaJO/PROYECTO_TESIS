aa548812deef553e752ceac0903af380fbd1b045
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2025-01-28 22:26:26 -0500

    Pruebas Admin-Conductor Casi-Completo

--- src/test/conductor.test.js
+++ src/test/conductor.test.js
@@ -30,7 +30,7 @@ const __dirname = path.dirname(__filename);
 
 describe('Pruebas de rutas del conductor', () => {
     const app = "https://proyecto-tesis-1.onrender.com";
-    // Prueba: Inicio de sesión del administrador
+    // Prueba del inicio de sesión del conductor
     // test('Debe iniciar sesión correctamente con credenciales válidas para un conductor', async () => {
     //     // Credenciales válidas del conductor
     //     const email = 'francishj369@gmail.com';
@@ -108,7 +108,7 @@ describe('Pruebas de rutas del conductor', () => {
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ // 
 // Lista de los estudiantes del conductor 
     // test('Debe listar los estudiantes de la ruta del conductor logueado', async () => {
-    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGMzOTdiZWQwMjljMjNkNjEzZGExYiIsImVtYWlsIjoiZnJhbmNpc2hqMzY5QGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzcyNDQ4NTYsImV4cCI6MTczNzI0ODQ1Nn0.fyOpjrxeBdYitB0pjDF6NyrmVN4xa86_3_ODJW67qrE'; // Reemplaza con un token válido para pruebas
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiZnJqb2FjMjMwM0BnbWFpbC5jb20iLCJyb2xlIjoiY29uZHVjdG9yIiwiaWF0IjoxNzM4MTE5MzM1LCJleHAiOjE3MzgxMjI5MzV9.kCbV5tGNzlQQ1kWfFCBcEc49klibUqNLMK73iF-EarI'; // Reemplaza con un token válido para pruebas
     
     //     const response = await request(app)
     //         .get('/api/lista/estudiantes')
@@ -138,7 +138,7 @@ describe('Pruebas de rutas del conductor', () => {
     //     expect(response.body).toHaveProperty('msg');
     // });
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-// Prueba de actualizar los datos del estudiante 
+// Prueba de actualizar los datos del estudiante por medio del ID  
 // test('Debe actualizar los datos de un estudiante', async () => {
 //     // Token válido para las pruebas
 //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGMzOTdiZWQwMjljMjNkNjEzZGExYiIsImVtYWlsIjoiZnJhbmNpc2hqMzY5QGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzcyNDQ4NTYsImV4cCI6MTczNzI0ODQ1Nn0.fyOpjrxeBdYitB0pjDF6NyrmVN4xa86_3_ODJW67qrE'; 
@@ -173,8 +173,8 @@ describe('Pruebas de rutas del conductor', () => {
     
 // Eliminar a un estudiante 
     // test('Debe eliminar un estudiante', async () => {
-    //     const token = 'token_de_prueba_valido'; // Reemplaza con un token válido para pruebas
-    //     const idEstudiante = 'id_estudiante'; // Reemplaza con un ID válido de estudiante
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiam9lbGZhMjMwQGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzgxMTc1MTEsImV4cCI6MTczODEyMTExMX0.vw_9bZYrTBJ4zwYlYBxX_osrZp0JvKhETatVNV7CRgs'; // Reemplaza con un token válido para pruebas
+    //     const idEstudiante = '679992db3729c48a2e556efb'; // Reemplaza con un ID válido de estudiante
     
     //     const response = await request(app)
     //         .delete(`/api/eliminar/estudiante/${idEstudiante}`)
@@ -191,15 +191,15 @@ describe('Pruebas de rutas del conductor', () => {
 // Actualizacion de la ubicacion de un conductor 
 // test('Debe actualizar la ubicación del conductor', async () => {
 //     // Generar un token válido para pruebas
-//     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGMzOTdiZWQwMjljMjNkNjEzZGExYiIsImVtYWlsIjoiZnJhbmNpc2hqMzY5QGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzcyNTI2MDksImV4cCI6MTczNzI1NjIwOX0.OiPYZw9aWiJOA0bvF003GZdrZoG5JoXOPQbx9IOpDW0'; // Token con el id del conductor
+//     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiam9lbGZhMjMwQGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzgxMTc1MTEsImV4cCI6MTczODEyMTExMX0.vw_9bZYrTBJ4zwYlYBxX_osrZp0JvKhETatVNV7CRgs'; // Token con el id del conductor
 
 //     // Enviar la solicitud de actualización
 //     const response = await request(app)
 //         .post('/api/actualizar/ubicacion')
 //         .set('Authorization', `Bearer ${token}`)
 //         .send({
-//             latitud: -0.139313,
-//             longitud: -78.505692
+//             latitud:  -0.193669,
+//             longitud: -78.483152
 //         });
 
 //     // Depuración: imprime la respuesta en caso de fallo
@@ -215,7 +215,7 @@ describe('Pruebas de rutas del conductor', () => {
 // Vizualizacion del perfil del conductor
     
     // test('Debe visualizar el perfil del conductor', async () => {
-    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGMzOTdiZWQwMjljMjNkNjEzZGExYiIsImVtYWlsIjoiZnJhbmNpc2hqMzY5QGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzcyNTI2MDksImV4cCI6MTczNzI1NjIwOX0.OiPYZw9aWiJOA0bvF003GZdrZoG5JoXOPQbx9IOpDW0'; // Reemplaza con un token válido para pruebas
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiam9lbGZhMjMwQGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzgxMTc1MTEsImV4cCI6MTczODEyMTExMX0.vw_9bZYrTBJ4zwYlYBxX_osrZp0JvKhETatVNV7CRgs'; // Reemplaza con un token válido para pruebas
     
     //     const response = await request(app)
     //         .get('/api/perfil/conductor')
@@ -233,116 +233,118 @@ describe('Pruebas de rutas del conductor', () => {
     
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 // Prueba para actualizar el perfil del conductor 
-    test('Debe actualizar el perfil del conductor', async () => {
-        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OGMzOTdiZWQwMjljMjNkNjEzZGExYiIsImVtYWlsIjoiZnJhbmNpc2hqMzY5QGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzcyNjAyMDEsImV4cCI6MTczNzI2MzgwMX0.0DWslZryU8XqTuH3FJSKSaor5bqfINAWi8N70WfGgEE'; // Reemplaza con un token válido para pruebas
-        // Ruta del archivo de imagen local
-            const filePath = path.join(__dirname, 'files', 'hm3.jpg');
+    // test('Debe actualizar el perfil del conductor', async () => {
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiam9lbGZhMjMwQGdtYWlsLmNvbSIsInJvbGUiOiJjb25kdWN0b3IiLCJpYXQiOjE3MzgxMTc1MTEsImV4cCI6MTczODEyMTExMX0.vw_9bZYrTBJ4zwYlYBxX_osrZp0JvKhETatVNV7CRgs'; // Reemplaza con un token válido para pruebas
+    //     // Ruta del archivo de imagen local
+    //         const filePath = path.join(__dirname, 'files', 'hm3.jpg');
         
-            // Verificar si el archivo existe antes de ejecutar la prueba
-            if (!fs.existsSync(filePath)) {
-              throw new Error(`El archivo ${filePath} no existe. Asegúrate de que esté en la ubicación correcta.`);
-            }
+    //         // Verificar si el archivo existe antes de ejecutar la prueba
+    //         if (!fs.existsSync(filePath)) {
+    //           throw new Error(`El archivo ${filePath} no existe. Asegúrate de que esté en la ubicación correcta.`);
+    //         }
         
-        const response = await request(app)
-            .patch('/api/actualizar/perfil/conductor')
-            .set('Authorization', `Bearer ${token}`)
-            .field('placaAutomovil', 'PHG2BY6')
-            .field('genero', 'Masculino')
-            .field('telefono', '0973645277')
-            .field('email', 'frjoac2303@gmail.com')
-            .attach('fotografiaDelConductor', filePath) // Archivo local
+    //     const response = await request(app)
+    //         .patch('/api/actualizar/perfil/conductor')
+    //         .set('Authorization', `Bearer ${token}`)
+    //         .field('placaAutomovil', 'PHG2BY6')
+    //         .field('telefono', '0973645277')
+    //         .field('email', 'frjoac2303@gmail.com')
+    //         .attach('fotografiaDelConductor', filePath) // Archivo local
 
+    //     console.log(response.body); // Para depuración
+    //     // Verifica que la respuesta sea exitosa
+    //     expect(response.status).toBe(200);
+                
+    // });
+//     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+// Registro de la asistencia trde 
+    // test('Debe registrar asistencia en la tarde', async () => {
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiZnJqb2FjMjMwM0BnbWFpbC5jb20iLCJyb2xlIjoiY29uZHVjdG9yIiwiaWF0IjoxNzM4MTE5MzM1LCJleHAiOjE3MzgxMjI5MzV9.kCbV5tGNzlQQ1kWfFCBcEc49klibUqNLMK73iF-EarI'; // Usa un token válido para pruebas
+        
+    //     // Obtener la fecha de hoy en formato YYYY-MM-DD
+    //     const fechaHoy = new Date().toISOString().split('T')[0];
+    
+    //     const response = await request(app)
+    //         .post('/api/tomar/asistencia/tarde')
+    //         .set('Authorization', `Bearer ${token}`)
+    //         .send({
+    //             estudiantes: [
+    //                 { estudiante: '6799983da1e6293231bb8a0a', asistio: true },
+    //                 { estudiante: '6799983da1e6293231bb8a0a', asistio: false }
+    //             ]
+    //         });
+    
+    //     console.log(response.body); // Para depuración
+    
+    //     // Verifica que la respuesta sea exitosa
+    //     expect(response.status).toBe(201);
+    //     expect(response.body.msg).toBe('Asistencia registrada exitosamente');
+    //     expect(response.body).toHaveProperty('asistencia');
+    // });
+    
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+    
+// Prueba de busqueda de asistencia por fecha 
+    // test('Debe buscar la asistencia por fecha', async () => {
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiZnJqb2FjMjMwM0BnbWFpbC5jb20iLCJyb2xlIjoiY29uZHVjdG9yIiwiaWF0IjoxNzM4MTE5MzM1LCJleHAiOjE3MzgxMjI5MzV9.kCbV5tGNzlQQ1kWfFCBcEc49klibUqNLMK73iF-EarI'; // Reemplaza con un token válido para pruebas
+    //     const fecha = '2025-01-29'; // Fecha de prueba
+    
+    //     const response = await request(app)
+    //         .get(`/api/buscar/asistencia/fecha/${fecha}`)
+    //         .set('Authorization', `Bearer ${token}`);
+    
+    //     console.log(response.body); // Para depuración
+    
+    //     // Verifica que la respuesta sea exitosa
+    //     expect(response.status).toBe(200);
+    //     expect(response.body).toHaveProperty('lista');
+    // });
+    
+    
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+    
+// Prueba para actualizar la lista de asistencia 
+    // test('Debe actualizar la lista de asistencia', async () => {
+    //     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiZnJqb2FjMjMwM0BnbWFpbC5jb20iLCJyb2xlIjoiY29uZHVjdG9yIiwiaWF0IjoxNzM4MTE5MzM1LCJleHAiOjE3MzgxMjI5MzV9.kCbV5tGNzlQQ1kWfFCBcEc49klibUqNLMK73iF-EarI'; // Reemplaza con un token válido para pruebas
+    //     const listaId = '67999b99a1e6293231bb8a25'; // Reemplaza con un ID válido de lista
+    //     const estudiantes = [
+    //         { estudiante: '6799983da1e6293231bb8a0a', asistio: true },
+    //         { estudiante: '6799983da1e6293231bb8a0a', asistio: false }
+    //     ]; // Datos de estudiantes para la prueba
+    
+    //     const response = await request(app)
+    //         .patch(`/api/actualizar/asistencia/${listaId}`)
+    //         .set('Authorization', `Bearer ${token}`)
+    //         .send({ estudiantes });
+    
+    //     console.log(response.body); // Para depuración
+    
+    //     // Verifica que la respuesta sea exitosa
+    //     expect(response.status).toBe(200);
+    //     expect(response.body.msg_actualizar_lista).toBe(
+    //         `La lista de la tarde con ID: ${listaId} se ha actualizado exitosamente`
+    //     );
+    //     expect(response.body.notificaciones.length).toBe(0); // Verifica que se hayan enviado notificaciones
+    // });
+    
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+    
+// Prueba para eliminar de la lista de asistencia 
+    test('Debe eliminar una lista de asistencia', async () => {
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3OTk5MGY3MzcyOWM0OGEyZTU1NmVmMyIsImVtYWlsIjoiZnJqb2FjMjMwM0BnbWFpbC5jb20iLCJyb2xlIjoiY29uZHVjdG9yIiwiaWF0IjoxNzM4MTE5MzM1LCJleHAiOjE3MzgxMjI5MzV9.kCbV5tGNzlQQ1kWfFCBcEc49klibUqNLMK73iF-EarI'; // Reemplaza con un token válido para pruebas
+        const listaId = '67999b99a1e6293231bb8a25'; // Reemplaza con un ID válido de lista
+    
+        const response = await request(app)
+            .delete(`/api/eliminar/asistencia/id/${listaId}`)
+            .set('Authorization', `Bearer ${token}`);
+    
         console.log(response.body); // Para depuración
+    
         // Verifica que la respuesta sea exitosa
         expect(response.status).toBe(200);
-                
-        });
-//     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-//     // REgistro de la asistencia trde 
-//     test('Debe registrar asistencia tarde', async () => {
-//         const token = 'token_de_prueba_valido'; // Reemplaza con un token válido para pruebas
-    
-//         const response = await request(app)
-//             .post('/api/tomar/asistencia/tarde')
-//             .set('Authorization', `Bearer ${token}`)
-//             .send({
-//                 cedulaEstudiante: '1234567890',
-//                 fecha: '2025-01-18'
-//             });
-    
-//         console.log(response.body); // Para depuración
-    
-//         // Verifica que la respuesta sea exitosa
-//         expect(response.status).toBe(200);
-//         expect(response.body.msg_asistencia_tarde).toBe('Asistencia tarde registrada correctamente');
-//     });
-    
-    
-    
-//     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    
-//     // Prueba de busqueda de asistencia por fecha 
-//     test('Debe buscar la asistencia por fecha', async () => {
-//         const token = 'token_de_prueba_valido'; // Reemplaza con un token válido para pruebas
-//         const fecha = '2025-01-18'; // Fecha de prueba
-    
-//         const response = await request(app)
-//             .get(`/api/buscar/asistencia/fecha/${fecha}`)
-//             .set('Authorization', `Bearer ${token}`);
-    
-//         console.log(response.body); // Para depuración
-    
-//         // Verifica que la respuesta sea exitosa
-//         expect(response.status).toBe(200);
-//         expect(response.body).toHaveProperty('asistencia');
-//     });
-    
-    
-//     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    
-//     // Peuba para actualizar la lista de asistencia 
-//     test('Debe actualizar la lista de asistencia', async () => {
-//         const token = 'token_de_prueba_valido'; // Reemplaza con un token válido para pruebas
-//         const listaId = 'id_lista_asistencia'; // Reemplaza con un ID válido de lista
-//         const estudiantes = [
-//             { estudiante: 'id_estudiante_1', asistio: true },
-//             { estudiante: 'id_estudiante_2', asistio: false }
-//         ]; // Datos de estudiantes para la prueba
-    
-//         const response = await request(app)
-//             .patch(`/api/actualizar/asistencia/${listaId}`)
-//             .set('Authorization', `Bearer ${token}`)
-//             .send({ estudiantes });
-    
-//         console.log(response.body); // Para depuración
-    
-//         // Verifica que la respuesta sea exitosa
-//         expect(response.status).toBe(200);
-//         expect(response.body.msg_actualizar_lista).toBe(
-//             `La lista de la mañana con ID: ${listaId} se ha actualizado exitosamente`
-//         );
-//         expect(response.body.notificaciones.length).toBeGreaterThan(0); // Verifica que se hayan enviado notificaciones
-//     });
-    
-//     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-    
-//     // Prueba para eliminar de la lista de asistencia 
-//     test('Debe eliminar una lista de asistencia', async () => {
-//         const token = 'token_de_prueba_valido'; // Reemplaza con un token válido para pruebas
-//         const listaId = 'id_lista_asistencia'; // Reemplaza con un ID válido de lista
-    
-//         const response = await request(app)
-//             .delete(`/api/eliminar/asistencia/id/${listaId}`)
-//             .set('Authorization', `Bearer ${token}`);
-    
-//         console.log(response.body); // Para depuración
-    
-//         // Verifica que la respuesta sea exitosa
-//         expect(response.status).toBe(200);
-//         expect(response.body.msg).toBe(
-//             `La lista de la mañana con ID: ${listaId}, se ha eliminado exitosamente`
-//         );
-//     });
-    // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+        expect(response.body.msg).toBe(
+            `La lista de la tarde con ID: ${listaId}, se ha eliminado exitosamente`
+        );
+    });
     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 });
