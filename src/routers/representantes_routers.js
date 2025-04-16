b827c0200e0e560ff93d14d71784a1e7d68e0e3f
mireya111 <mireya.garcia@epn.edu.ec>
2025-04-15 23:17:21 -0500

    Eliminación de controladores innecesarios (el nuevo enfoque es que el móvil se encarga de las notificaciones): "Ver notificaciones" y "Alerta de llegada".

--- src/routers/representantes_routers.js
+++ src/routers/representantes_routers.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import {verificacionRepresentanteRol, verificacionToken} from '../middlewares/autho.js'
-import {RegistroDeRepresentantes, ConfirmacionCorreo, ActualizarPasswordRepresentante, ConductorInfo, VisuallizarPerfil, EliminarCuentaRepresentante, AlertaLlegadaConductor, VerNotificaciones, ActualizarPerfilRepresentante} from '../controllers/representantes_controller.js'
+import {RegistroDeRepresentantes, ConfirmacionCorreo, ActualizarPasswordRepresentante, ConductorInfo, VisuallizarPerfil, EliminarCuentaRepresentante, ActualizarPerfilRepresentante} from '../controllers/representantes_controller.js'
 import {validacionesRepresentantes, validacionesActualizarPerfilRepresentante, validarContraseniaNueva} from '../middlewares/validaciones.js'
 const router = Router()
 //Rutas publicas
@@ -12,8 +12,6 @@ router.patch('/actualizar/contrasenia/representante', verificacionToken, verific
 router.get("/info/conductor", verificacionToken, verificacionRepresentanteRol, ConductorInfo);
 router.get("/perfil/representante", verificacionToken, verificacionRepresentanteRol, VisuallizarPerfil);
 router.delete("/eliminar/cuenta/representante", verificacionToken, verificacionRepresentanteRol, EliminarCuentaRepresentante);
-router.get("/alerta/llegada/conductor", verificacionToken, verificacionRepresentanteRol, AlertaLlegadaConductor);
-router.get("/notificaciones/representante", verificacionToken, verificacionRepresentanteRol, VerNotificaciones);
 router.patch("/actualizar/perfil/representante", verificacionToken, verificacionRepresentanteRol, validacionesActualizarPerfilRepresentante, ActualizarPerfilRepresentante);
 
 export default router
